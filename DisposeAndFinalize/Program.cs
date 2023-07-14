90a87010b042d1059053ed68d43f6bac0458bf97
jol96 <jack1996oloughlin@gmail.com>
2023-07-14 12:44:58 +0100

    Dispose method example

---
+++ DisposeAndFinalize/Program.cs
@@ -0,0 +1,70 @@
+﻿public class Program
+{
+    // Dispose() is used to handle unmanaged resources like DB connections, file streams etc.
+    // The Common Language Runtime handles managed resources like objects
+
+    public static void Main(string[] args)
+    {
+        // Approach 1: Call Dispose() manually
+        var fileReader = new FileReader("..\\..\\..\\TextFile1.txt");
+        fileReader.ReadLines();
+        fileReader.Dispose();
+
+        // Approach 2: Dispose() is automatically called with using statemment
+        using (var fileReader2 = new FileReader("..\\..\\..\\TextFile1.txt"))
+        { 
+            fileReader2.ReadLines(); 
+        }
+
+        // Approach 3: 
+        try
+        {
+            fileReader.ReadLines();
+        }
+        finally 
+        { 
+            fileReader?.Dispose(); 
+        }
+    }
+}
+
+public class FileReader : IDisposable
+{
+    private StreamReader _stream;
+    private readonly string _path;
+
+    public FileReader(string path)
+    { 
+        _path = path; 
+    }
+
+    public string ReadLine()
+    {
+        _stream = new StreamReader(_path);
+        var line = _stream.ReadLine();
+        Console.WriteLine($"Line: {line}");
+        return line;
+    }
+
+    public List<string> ReadLines()
+    {
+        _stream = new StreamReader(_path);
+        var lines = new List<string>();
+        string line;
+        while ((line = _stream.ReadLine()) != null)
+        {
+            lines.Add(line);
+        }
+        foreach (var l in lines)
+        {
+            Console.WriteLine($"Line: {l}");
+        }
+        return lines;
+    }
+
+
+    public void Dispose()
+    {
+        _stream.Dispose();
+    }
+}
