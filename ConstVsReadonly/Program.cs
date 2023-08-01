6d9038f5d7de0f8f9f9bfa86db5fa7f59cc9d05a
jol96 <jack1996oloughlin@gmail.com>
2023-08-01 20:16:15 +0100

    ref vs out

---
+++ ConstVsReadonly/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public const int MyConstant = 10; // Compile-time constant 
+    public readonly int MyReadOnlyField;
+
+    public Program(int value)
+    {
+        MyReadOnlyField = value; // Runtime initialization 
+    }
+
+    static void Main(string[] args)
+    {
+        Program program = new(3);
+        Console.WriteLine($"MyReadOnlyField: {program.MyReadOnlyField}"); // 3 
+        Console.WriteLine($"MyConstant: {MyConstant}"); // 10 
+    }
+
+}
---
+++ RefVsOut/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    // Ref example
+    public static void RefExample(ref int id)
+    {
+        id += 1;
+    }
+
+    // Out Example
+    public static void OutExample(out int id)
+    {
+        id = 20;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Ref example
+        int i = 1;
+        Console.WriteLine("Previous value of integer i:" + i.ToString());
+        RefExample(ref i);
+        Console.WriteLine("Current value of integer i:" + i.ToString());
+
+        // Out Example
+        int k = 15;
+        Console.WriteLine("Previous value of integer k:" + k.ToString());
+        OutExample(out k);
+        Console.WriteLine("Current value of integer k:" + k.ToString());
+    }
+}
